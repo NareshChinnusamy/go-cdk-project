8c86a04c804e75e383ea600d517f233a873f4d45
Naresh <naresh@breezeware.net>
2023-01-03 16:02:04 +0530

    Initial Commit

--- go-cdk-project.go
+++ go-cdk-project.go
@@ -2,67 +2,51 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsecs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
 	"github.com/aws/constructs-go/constructs/v10"
-	// "github.com/aws/jsii-runtime-go"
 )
 
-type GoCdkProjectStackProps struct {
+type StackProps struct {
 	awscdk.StackProps
 }
 
-func NewGoCdkProjectStack(scope constructs.Construct, id string, props *GoCdkProjectStackProps) awscdk.Stack {
+func S3BucketStack(scope constructs.Construct, id string, props StackProps) awscdk.Stack {
 	var sprops awscdk.StackProps
-	if props != nil {
+	bucketName := "s3-demo-go-bucket"
+	removalPolicy := awscdk.RemovalPolicy_DESTROY
+	if &props != nil {
 		sprops = props.StackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+	awss3.NewBucket(stack, &bucketName, &awss3.BucketProps{BucketName: &bucketName, RemovalPolicy: removalPolicy})
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("GoCdkProjectQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	return stack
+}
 
+func EcsClusterStack(scope constructs.Construct, id string, props StackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	clusterName := "go-demo-cluster"
+	containerInsights := false
+	if &props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+	awsecs.NewCluster(stack, &clusterName, &awsecs.ClusterProps{ClusterName: &clusterName, ContainerInsights: &containerInsights})
 	return stack
 }
 
 func main() {
 	app := awscdk.NewApp(nil)
 
-	NewGoCdkProjectStack(app, "GoCdkProjectStack", &GoCdkProjectStackProps{
-		awscdk.StackProps{
-			Env: env(),
-		},
-	})
+	S3BucketStack(app, "S3BucketStack", StackProps{})
+
+	EcsClusterStack(app, "EcsClusterStack", StackProps{})
 
 	app.Synth(nil)
 }
 
-// env determines the AWS environment (account+region) in which our stack is to
-// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
 	return nil
-
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module go-cdk-project
 go 1.16
 
 require (
-  github.com/aws/aws-cdk-go/awscdk/v2 v2.35.0
-  github.com/aws/constructs-go/constructs/v10 v10.0.5
-  github.com/aws/jsii-runtime-go v1.29.0
+	github.com/aws/aws-cdk-go/awscdk/v2 v2.58.1
+	github.com/aws/constructs-go/constructs/v10 v10.1.189
 )
